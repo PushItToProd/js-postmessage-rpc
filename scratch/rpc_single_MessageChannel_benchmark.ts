8f45b83fb26eddf454bfd403afcb1c673cfb139f
pushittoprod <31714132+PushItToProd@users.noreply.github.com>
2025-10-24 07:47:46 -0700

    benchmark using a single MessageChannel

---
+++ scratch/rpc_single_MessageChannel_benchmark.ts
@@ -0,0 +1,28 @@
+// Benchmark the cost of using one MessageChannel for all requests. On my Mac
+// with an M2 Pro, this averages about 0.005ms per operation in Node, 0.008ms/op
+// in Firefox, and 0.020ms/op in Chrome.
+import { RpcServer, RpcTransport } from "./rpc_single_MessageChannel.ts";
+
+const NUM_ITERATIONS = 100_000;
+
+async function main() {
+  const {port1, port2} = new MessageChannel();
+
+  const s = new RpcServer(port1);
+  const t = new RpcTransport(port2);
+
+  const benchmarkStart = performance.now();
+  for (let i = 0; i < NUM_ITERATIONS; i++) {
+    let _response = await t.makeCall("ping");
+  }
+  const benchmarkEnd = performance.now();
+  const benchmarkTimeMs = benchmarkEnd - benchmarkStart;
+  const avgTimeMs = benchmarkTimeMs / NUM_ITERATIONS;
+  console.log("Ran %d iterations in %f ms (average: %f ms)", NUM_ITERATIONS, benchmarkTimeMs, avgTimeMs);
+
+  port1.close();
+  port2.close();
+}
+
+await main();
+
